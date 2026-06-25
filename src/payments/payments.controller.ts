1eedc93a358e24e7f10d0415d22644dee6070939
pietrodeveloper <devpietro2024@gmail.com>
2026-06-25 13:04:50 -0500

    First commit

---
+++ src/payments/payments.controller.ts
@@ -0,0 +1,38 @@
+import { Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { PaymentsService } from './payments.service';
+import { PaymentSessionDTO } from './dto/payment-session.dto';
+import type { Request, Response } from 'express';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+
+@Controller('payments')
+export class PaymentsController {
+  constructor(private readonly paymentsService: PaymentsService) {}
+
+  // @Post('create-payment-session')
+  @MessagePattern('create.payment.session')
+  createPaymentSession(@Payload() paymentSessionDto: PaymentSessionDTO){
+    return this.paymentsService.createPaymentSesssion(paymentSessionDto);
+  }
+
+  @Get('success')
+  success(){
+    return {
+      ok: true,
+      message: 'Payment successful'
+    };
+  }
+
+  @Get('cancel')
+  cancel(){
+    return {
+      ok: false,
+      message: 'Payment cancelled'
+    };
+  }
+
+  @Post('webhook')
+  async stripeWebhook(@Req() req: Request, @Res() res: Response){
+    return this.paymentsService.stripeWebhook(req, res);
+  }
+
+}
---
+++ src/payments/payments.service.ts
@@ -0,0 +1,89 @@
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import { envs, NATS_SERVICE } from 'src/config';
+import Stripe from 'stripe';
+import { PaymentSessionDTO } from './dto/payment-session.dto';
+import { Request, Response } from 'express';
+import { ClientProxy } from '@nestjs/microservices';
+
+@Injectable()
+export class PaymentsService {
+    private readonly stripe = new Stripe(envs.stripeSecret);
+    private readonly logger = new Logger('Payment');
+
+    constructor(
+        @Inject(NATS_SERVICE) private readonly client: ClientProxy
+    ){}
+
+    async createPaymentSesssion(paymentSessionDto: PaymentSessionDTO){
+        const { currency, items, orderId } = paymentSessionDto;
+
+        const lineItems = items.map(item => {
+            return {
+                price_data: {
+                    currency: currency,
+                    product_data: {
+                        name: item.name
+                    },
+                    unit_amount: Math.round(item.price * 100) 
+                },
+                quantity: item.quantity
+            }
+        })
+
+        const session = await this.stripe.checkout.sessions.create({
+            payment_intent_data: {
+                metadata: {
+                    orderId: orderId
+                }
+            },
+            line_items: lineItems,
+            mode: 'payment',
+            success_url: envs.stripeSuccessUrl,
+            cancel_url: envs.stripeCancelUrl
+        });
+
+        // return session;
+
+        return {
+            cancelUrl: session.cancel_url,
+            succesUrl: session.success_url,
+            url: session.url
+        }
+    }
+
+    async stripeWebhook(req: Request, res: Response){ 
+        const sig = req.headers['stripe-signature'] as string;
+        let event: Stripe.Event;
+        const endpointSecret = envs.stripeEndpointSecret;
+
+        try {
+            event = this.stripe.webhooks.constructEvent(
+                req['rawBody'],
+                sig,
+                endpointSecret
+            );
+        } catch (err) {
+            const message = err instanceof Error ? err.message : 'Unknown error';
+            res.status(400).send(`Webhook Error: ${message}`);
+            return;
+        }``
+
+        switch(event.type){
+            case 'charge.succeeded':
+                const chargeSucceeded = event.data.object;
+                const payload = {
+                    stripePaymentId: chargeSucceeded.id,
+                    orderId: chargeSucceeded.metadata.orderId,
+                    receiptUrl: chargeSucceeded.receipt_url,
+                }
+
+                // this.logger.log({payload});
+                this.client.emit('payment.succeeded', payload);
+                break;
+            default:
+                console.log(`Event ${event.type} not handled`);
+        }
+
+        return res.status(200).json({ sig });
+    }
+}
